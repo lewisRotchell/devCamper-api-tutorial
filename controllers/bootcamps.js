195d04f0a6e5070d4c7da2ecc0e427e0d67022c0
Lewis Rotchell <rotchell92@gmail.com>
2021-03-06 11:10:20 +0000

    Added courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,12 +1,74 @@
 const ErrorResponse = require("../utils/errorResponse");
 const catchAsync = require("../middleware/async");
 const Bootcamp = require("../models/Bootcamp");
+const geocoder = require("../utils/geocoder");
 
 exports.getBootcamps = catchAsync(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  const reqQuery = { ...req.query };
+
+  //Fields to exclude
+  const removeFields = ["select", "sort", "limit", "page"];
+
+  //Loop over removeFields and delete form reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
+
+  //Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  //Pagination - use this in other projects
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  //Execute query
+  query = query.skip(startIndex).limit(limit);
+
+  //Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  const bootcamps = await query;
   res.status(200).json({
     success: true,
     results: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
@@ -54,12 +116,39 @@ exports.updateBootcamp = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteBootcamp = catchAsync(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
+
   res.status(200).json({ success: true });
+});
+
+exports.getBootcampsInRadius = catchAsync(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi or 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    results: bootcamps.length,
+    data: bootcamps,
+  });
 });
---
+++ controllers/courses.js
@@ -0,0 +1,104 @@
+const ErrorResponse = require("../utils/errorResponse");
+const catchAsync = require("../middleware/async");
+const Course = require("../models/course");
+const Bootcamp = require("../models/Bootcamp");
+
+exports.getCourses = catchAsync(async (req, res, next) => {
+  let query;
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find().populate({
+      path: "bootcamp",
+      select: "name, description",
+    });
+  }
+
+  const courses = await query;
+  res.status(200).json({
+    success: true,
+    results: courses.length,
+    data: courses,
+  });
+});
+
+exports.getCourse = catchAsync(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name, description",
+  });
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+exports.addCourse = catchAsync(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Bootcamp not found with id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+exports.updateCourse = catchAsync(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(
+        `Course not found with id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+exports.deleteCourse = catchAsync(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(
+        `Course not found with id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
